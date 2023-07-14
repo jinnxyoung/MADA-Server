9168aeeed66f98a1c824007089b833de166a4498
jinnxyoung <wlsdud7722@naver.com>
2023-07-14 20:00:00 +0900

    Initial commit

---
+++ src/test/java/com/umc/mada/MadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.mada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
